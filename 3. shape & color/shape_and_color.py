406928f18505273df6d61cf971bc6091b6beb045
NishadiSS <nishadisasanthika260@gmail.com>
2023-12-29 21:25:10 +0530

    color and shapes

---
+++ 3. shape & color/shape_and_color.py
@@ -0,0 +1,31 @@
+#shape(shapename) -> Should be 'arrow', 'classic', 'turtle' or 'circle'
+#color(Color name) -> Changes the color of the turtle’s pens
+
+from turtle import *
+shape("turtle")
+color("red")
+pensize(5)
+
+#letter 'T'
+forward(200)
+backward(100)
+right(90)
+forward(200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
